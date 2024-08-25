83a6cfbf6f254087c96f27f001c2b4e7a1ec5cdd
The-greenpanter <judpenaca@unal.edu.co>
2024-08-25 17:59:40 -0500

    First request using postman

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, {urlencoded} from "express";
+import cors from "cors";
 import connection from "./database/connection.js";
 
 // Load environment variables
@@ -18,11 +19,31 @@ connection();
 const app = express();
 const port  = process.env.PORT || 300;
 
-// Cors Config
+// Cors Config for the HTTPs request
 
+app.use(cors());
+ // app.use(cors({origin:'*',methods: 'GET', 'PUT', 'HEAD', 'PATCH', 'POST', 'DELETE' }));
 
+//decode data from the HTTPs request received from apache
 
-// Server config
+app.use(urlencoded({extended: true}));
+
+// Route Config
+
+    //Test route
+
+    app.get('/test-route',(req, rest)=>{
+        return rest.status(200).json(
+            {
+                'id':1,
+                'name':'Green Pa nter',
+                'username':'PanterTest'
+
+            }
+        )
+    })
+
+// Server node config
 
 app.listen(port, () => {
     console.log("Server Node running Port:", port);
